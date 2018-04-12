47f5632c2a2f59979294c22a4e9cecee3d321b04
niubilityxiesong <563527588@qq.com>
2018-04-12 14:45:27 +0800

    P5

--- src/main/java/practice06/Student.java
+++ src/main/java/practice06/Student.java
@@ -1,4 +1,23 @@
 package practice06;
 
-public class Student {
+public class Student extends Person{
+
+    int klass;
+
+    public Student(String array, int num, int number){
+        super(array, num);
+        klass = number;
+    }
+
+    int getKlass(){
+        return klass;
+    }
+    String introduce(){
+        String result = super.introduce();
+
+        result += " I am a Student. I am at Class ";
+        result += String.valueOf(klass);
+        result += ".";
+        return result;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,36 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    int age, klass;
+    String name;
+
+    public Teacher(String array, int num, int number){
+        super(array, num);
+        age = super.getAge();
+        name = super.getName();
+        klass = number;
+    }
+    public Teacher(String array, int num){
+        super(array, num);
+        age = super.getAge();
+        name = super.getName();
+        klass = -1;
+    }
+
+    int getKlass(){
+        return klass;
+    }
+    String introduce(){
+        String result = super.introduce();
+        if(klass > 0){
+            result += " I am a Teacher. I teach Class ";
+            result += String.valueOf(klass);
+            result += ".";
+        }
+        else {
+            result += " I am a Teacher. I teach No Class.";
+        }
+        return result;
+    }
 }
