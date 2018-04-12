45e4227c5df1196549384aa729279501fd93bb58
niubilityxiesong <563527588@qq.com>
2018-04-12 12:01:38 +0800

    P3

--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,19 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    int age;
+    String name = new String();
+
+    public Worker(String array, int num){
+        super(array,num);
+        age = super.age;
+        name = super.name;
+        super.getAge();
+        super.getName();
+    }
+
+    String introduce(){
+        String result = "I am a Worker. I have a job.";
+        return result;
+    }
 }
